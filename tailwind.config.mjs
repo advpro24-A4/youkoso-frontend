8d52beb30187ce3597c7b29149cfcfa50ce13205
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-04-17 09:59:26 +0700

    Create server mjs and tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["selector"],
+  mode: "jit",
+  content: [
+    './app/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
